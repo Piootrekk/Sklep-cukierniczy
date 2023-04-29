aa5c2edc87cb345faed230dcf577752245448085
DzaneQ <78421984+DzaneQ@users.noreply.github.com>
2023-04-29 19:59:40 +0200

    Wersja 2.1
    No rany to powinno być zaktualizowane we wtorek

--- Towar.sql
+++ Towar.sql
@@ -1,7 +1,7 @@
 -- Wprowadzaæ po insertach z tabeli: [Produkt] oraz [Zamowienie] !!!
 -- £¹czenie liczb reprezentuj¹cych ID produktu i zamówienia
 
-insert into Towar values (1, 3, 7);
-insert into Towar values (2, 7, 1);
-insert into Towar values (3, 5, 7);
-insert into Towar values (4, 5, 2);
+insert into Towar values (3, 7);
+insert into Towar values (7, 1);
+insert into Towar values (5, 7);
+insert into Towar values (5, 2);
