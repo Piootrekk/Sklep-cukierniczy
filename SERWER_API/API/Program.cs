bb8a44dab74880b9aaad4ed676827701871c96d9
Kowiel <86078928+Kowiel@users.noreply.github.com>
2023-05-02 19:48:11 +0200

    Połączenie Frontendu z Backendem
    Na razie doś tragiczne połączenie ale komunikacja jest i przesyła dane. Następne są formularze do zrobienia.

--- SERWER_API/API/Program.cs
+++ SERWER_API/API/Program.cs
@@ -16,6 +16,15 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IAutenticationService, AutenticationService>();
 
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 
@@ -25,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
